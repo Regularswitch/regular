54b954e7538ccdc9a61806583beb045ade84869c
Regularswitch <44267111+Regularswitch@users.noreply.github.com>
2024-01-23 13:17:34 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import React from 'react';
+import Head from 'next/head';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "http://schema.org",
+              "@type": "Organization",
+              "name": "RSW Regular Switch",
+              "url": "https://regularswitch.com/",
+              "contactPoint": [
+                {
+                  "@type": "ContactPoint",
+                  "telephone": "+55 11 94540-8448",
+                  "contactType": "customer service"
+                }
+              ]
+            }
+          `}
+        </script>
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;
